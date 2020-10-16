93234972cf150f979925c902fa72ad0013a2746d
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-16 16:06:25 -0700

    label process, add random ID generator

--- server.js
+++ server.js
@@ -8,36 +8,39 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Data Parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Arrays TBD
 
 //routes and redirects
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+//get html files
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/develop/public/index.html"));
 });
 
-app.get("/notes, function"(req, res) {
-    res.Sendfile(path.join(__dirname, "public/notes.html"));
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/develop/public/notes.html"));
 })
 
 //Create New Notes
-app.post("/api/notes/new", function(req,res) {
-    const noteBody = 
+app.post("/api/notes", function (req, res) {
+    const numberID = Math.floor((Math.random() * 100))
+    const noteBody =
     {
         id: numberID,
         title: req.body.title,
         note: req.body.text,
-    }
+    };
     console.log(noteBody);
+
+    
     // noteBody.routeName = noteBody.
     // noteBody.push(noteBody);
     // res.json(noteBody);
 });
 
 // start server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT http://localhost:" + PORT)
 })
