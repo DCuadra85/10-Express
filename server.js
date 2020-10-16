822d12b20c969abfa8b5def7ff029c5526e4490f
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-16 09:02:59 -0700

    define notes taken on add function

--- server.js
+++ server.js
@@ -16,7 +16,25 @@ app.use(express.json());
 //routes and redirects
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes, function"(req, res) {
+    res.Sendfile(path.join(__dirname, "public/notes.html"));
+})
+
+//Create New Notes
+app.post("/api/notes/new", function(req,res) {
+    const noteBody = 
+    {
+        id: numberID,
+        title: req.body.title,
+        note: req.body.text,
+    }
+    console.log(noteBody);
+    // noteBody.routeName = noteBody.
+    // noteBody.push(noteBody);
+    // res.json(noteBody);
 });
 
 // start server
