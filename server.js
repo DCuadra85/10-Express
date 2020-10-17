0b674f5a194254d3c42b932e9bf36c4942aca9bf
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-16 17:23:34 -0700

    note creation function

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require("express");
+const { fstat } = require("fs");
 const path = require("path")
 
 // Express setup
@@ -33,8 +34,27 @@ app.post("/api/notes", function (req, res) {
         note: req.body.text,
     };
     console.log(noteBody);
+   
+    fs.readFile("./develop/db/db.json", "utf8", function (err, res){
+        if (err) {
+            console.log("Error:" + err);
+        }
+        const mainNotes = JSON.parse(res);
+        //console.log("mainNotes"+mainNotes)
+        mainNotes.push(noteBody);
+        console.log("notes:" + JSON.stringify(mainNotes));
+    })
 
-    
+    fs.writeFile("./develop/db.json", JSON.stringify(mainNotes, null, 2), function(err){
+        if (err){
+            console.log("Error:" + err);
+        }
+        else{
+            res.json(mainNotes);
+            console.log("Note saved.");
+        }
+    })
+
     // noteBody.routeName = noteBody.
     // noteBody.push(noteBody);
     // res.json(noteBody);
