8cee675d80bccd510a34bcdb89826822a0ab1df1
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-16 17:28:44 -0700

    file paths acting strange, move files and reload package.json files

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+app.use(express.static('public'));
 
 // Data Parsing
 app.use(express.urlencoded({ extended: true }));
